28f7d8a79e5aac548fb428a1bf3dc38f61a253d8
alicjakarczewsk <90188020+alicjakarczewska@users.noreply.github.com>
2021-10-29 20:12:50 +0000

    PS4

--- main.py
+++ main.py
@@ -13,7 +13,6 @@
 
 import spacy
 nlp=spacy.load("en_core_web_sm")
-# print(nlp)
 
 # Parse text through the `nlp` model
 my_text = """The first published stemmer was written by Julie Beth Lovins in 1968.[1] This paper was remarkable for its early date and had great influence on later work in this area. Her paper refers to three earlier major attempts at stemming algorithms, by Professor John W. Tukey of Princeton University, the algorithm developed at Harvard University by Michael Lesk, under the direction of Professor Gerard Salton, and a third algorithm developed by James L. Dolby of R and D Consultants, Los Altos, California.
@@ -25,75 +24,60 @@ Many implementations of the Porter stemming algorithm were written and freely di
 The Paice-Husk Stemmer was developed by Chris D Paice at Lancaster University in the late 1980s, it is an iterative stemmer and features an externally stored set of stemming rules. The standard set of rules provides a 'strong' stemmer and may specify the removal or replacement of an ending. The replacement technique avoids the need for a separate stage in the process to recode or provide partial matching. Paice also developed a direct measurement for comparing stemmers based on counting the over-stemming and under-stemming errors.
 """
 
-# # my_doc = nlp(my_text)
-# my_doc = nlp(my_text.replace("\n", ""))
-# # Removing StopWords and punctuations
-# my_doc_cleaned = [token for token in my_doc if not token.is_stop and not token.is_punct]
+my_doc = nlp(my_text.replace("\n", ""))
+# Removing StopWords and punctuations
+my_doc_cleaned = [token for token in my_doc if not token.is_stop and not token.is_punct]
 
-# f = open("slownik.txt", "w")
-# for token in my_doc_cleaned:
-#   f.write(token.text+"\n")
-# f.close()
-
-f = open("slownik.txt", "r")
-
-my_dict = []
-read_file = f.read()
-for word in read_file:
-  my_dict.append(word)
+f = open("slownik.txt", "w")
+for token in my_doc_cleaned:
+  f.write(token.text+"\n")
 f.close()
 
-changed_dict = my_dict
-
+changed_dict=[]
+with open('slownik.txt','r') as file: 
+    for line in file:   
+        for word in line.split():         
+            changed_dict.append(word) 
 
 # losowanie 20% slow do zmiany
 import random
 
 bef_indices = [0]
-for i in range(1, len(my_dict)):
+for i in range(1, len(changed_dict)):
   bef_indices.append(bef_indices[i-1] + 1)
 
 percentage = 0.2
-k = len(my_dict)*percentage 
+k = len(changed_dict)*percentage 
 indicies = random.sample(bef_indices, int(k))
 
 import string
+
+def replace(s, position, character):
+    return s[:position] + character + s[position+1:]
 
 def charge_char_in_word(word):
   word[random.randrange(len(word))] = random.choice(string.ascii_letters)
 
 # zmiana w wyrazach pod wylosowanymi indeksami (1-3 modyfikacje)
-
 for i in indicies:  
-  how_many = random.randrange(3)
-  # zmiana znaku
-  changed_dict[i] = charge_char_in_word(changed_dict[i])
+  how_many = random.randrange(3)  
+  # dodanie znaku  
+  character = random.choice(string.ascii_letters)
+  word = changed_dict[i]
+  changed_dict[i] = ''.join((word, character))
   # usuniecie znaku
   if how_many > 0:
-    changed_dict[i] = changed_dict[:-1]
-  # dodanie znaku
+    changed_dict[i] = changed_dict[i][:-2]
+  # zmiana znaku
   if how_many > 1:
-    changed_dict[i] = changed_dict + random.choice(string.ascii_letters)
-
+    character = random.choice(string.ascii_letters)
+    word = changed_dict[i][:-1]
+    changed_dict[i] = ''.join((word, character))
+  
 f = open("przyklad_z_bledami.txt", "w")
-for token in changed_dict:
-  f.write(token.text+"\n")
+for word in changed_dict:
+  f.write(word+"\n")
 f.close()
-
-
-
-# print(len(my_doc))
-# print(len(my_doc_cleaned))
-# for token in my_doc_cleaned:
-#   if token.text == "\n":
-#     print("A")
-#   if token.text == " ":
-#     print("B")
-
-# f = open("slownik.txt", "w")
-# for token in my_doc_cleaned:
-#   f.write(token.text+"\n")
-# f.close()
 
 
 
